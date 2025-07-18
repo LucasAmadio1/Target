c2c206c0e48e60b321accb9894c9004f79855ff2
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-07-17 22:03:24 -0300

    input text / currency input

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,14 +1,32 @@
 import { View } from "react-native";
+import { Button } from "@/components/Button";
+import { CurrencyInput } from "@/components/CurrencyInput";
+import { Input } from "@/components/Input";
 import { PageHeader } from "@/components/PageHeader";
 
 export default function Target() {
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <PageHeader
-        title="Apple Watch"
-        subtitle="A cada valor guardado você fica mais próximo da sua meta. Se esforce para guardar e evitar retirar."
+        title="Meta"
+        subtitle="Economize para alcançar sua meta financeira."
         rightButton={{ icon: "edit", onPress: () => {} }}
       />
+
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <Input
+          label="Nome da meta"
+          placeholder="Ex: Viagem para praia, Apple Watch"
+        />
+
+        <CurrencyInput
+          label="Valor alvo (R$)"
+          placeholder="0,00"
+          value={2400.03}
+        />
+
+        <Button title="Salvar" />
+      </View>
     </View>
   );
 }
